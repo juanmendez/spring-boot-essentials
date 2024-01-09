8d00de34d40015c1c51e97b3f3a5634bc3cd1a94
Juan Mendez <juan.mendez1@grainger.com>
2024-01-08 20:22:17 -0600

    04: set db in an external server
    Through Docker we are able to run the database in its own server

--- spring-boot-essentials/build.gradle.kts
+++ spring-boot-essentials/build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("com.h2database:h2")
+    implementation("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
